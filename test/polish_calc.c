65428f53aa15a3c8833719d18c020841652c5bf4
Andrew from rosbt <help@rosbt.com>
2015-05-27 14:27:14 +0600

    20150527

--- test/polish_calc.c
+++ test/polish_calc.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAXOP 100
 #define NUMBER '0'
-#define MAXVAL 100;
+#define MAXVAL 100
+
+#define BUFSIZE 100
 
 int sp = 0;
 double val [ MAXVAL ];
+char buf [ BUFSIZE ]; //buffer for ungetch
+int bufp = 0; //next free position for ungetch
 
 int getop ( char [] );
-void push ( double );
+void push_my ( double );
 double pop ( void );
+
+int getch ( void );
+void ungetch ( int );
+int getop ( char s [] );
+
 
 void main ()
 
@@ -22,22 +32,22 @@ void main ()
     while ( ( type = getop ( s ) ) != EOF ){
         switch ( type ){
         case NUMBER:
-            push ( atof ( s ) );
+            push_my ( atof ( s ) );
             break;
         case '+':
-            push ( pop () + pop () );
+            push_my ( pop () + pop () );
             break;
         case '*':
-            push ( pop () * pop () );
+            push_my ( pop () * pop () );
             break;
         case '-':
             op2 = pop ();
-            push ( pop () - op2 );
+            push_my ( pop () - op2 );
             break;
         case '/':
             op2 = pop();
             if ( op2 != 0.0 )
-                push ( pop () / op2 );
+                push_my ( pop () / op2 );
             else 
                 printf ( "Error, division by zero.\n" );
             break;
@@ -52,12 +62,81 @@ void main ()
 }
 
 
-void push ( double f )
+void push_my ( double f )
 
 {
     if ( sp < MAXVAL )
-        
+        val [ sp++ ] = f;
+    else 
+        printf ( "Error:stack is full. %g not contain.\n", f );
 
+}
+
+double pop ( void ) 
+
+{
+
+    if ( sp > 0 )
+        return ( val [ --sp ] );
+    else 
+        printf ( "Error. Stack is empty.\n" );
+
+    return 0.0;
 
 
 }
+
+int getop ( char s [] )
+
+{
+    int i = 0;
+    int  c = 0;
+    
+    while (  s [ 0 ] = ( c =  getch () ) == ' ' || c  == '\t'  ) 
+        ;
+    s [ 1 ] = '\0';
+    
+    if ( ! isdigit ( c ) && c != '.' )
+        return c; /* is not a number */
+        
+    i = 0;
+    
+    if ( isdigit ( c ) )
+        while ( isdigit ( s [ ++i ] = c = getch () ) )
+            ;
+    if ( c == '.' )
+        while ( isdigit ( s [ ++i ] = c = getch () ) )
+            ;
+    s [ i ] = '\0';
+    if ( c != EOF )
+        ungetch ( c );
+        
+    return NUMBER;
+
+}
+
+int getch ( void )
+
+{
+    return ( ( bufp > 0 ) ? ( buf [ --bufp ] ) : getchar () );
+
+}
+
+void ungetch ( int c )
+
+{
+    if ( bufp >= BUFSIZE )
+        printf ( "ungetch: too much symbols.\n" );
+    else 
+        buf [ bufp++ ] = c;
+    
+
+}
+
+
+
+
+
+
+
+
