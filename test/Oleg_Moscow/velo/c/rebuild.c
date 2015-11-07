aa22e79287078589ee4484de51445ef1f395a678
Andrew0snake <andrew000snake@gmail.com>
2015-11-08 05:12:45 +0600

    20151108 home I

--- test/Oleg_Moscow/velo/c/rebuild.c
+++ test/Oleg_Moscow/velo/c/rebuild.c
@@ -5,6 +5,7 @@
 void clean_str ( char str [] );
 void del_sys_symb ( char str_to [], char str_from [] );
 void put_str ( char str [] );
+void clean_first_string ( char str_to [], char str_from [] );
 
 void main () {
 
@@ -20,43 +21,50 @@ void main () {
     short int index_of_symbol = 0;
     short int sc = 0;
     short int prev = 0;
+    char concat = 0;
 
     source_file = fopen ( "source_file", "r" );
     
     for ( sc = 0; sc < 50; sc++ ){
-        while ( fgets ( str1, 400, source_file) != NULL ){
-            printf ( "getted string = %s,\n", str1 );
+        while ( fgets ( str1, 800, source_file) != 0 ){
             if ( str1 [ 0 ] == '<' ) { 
-//                if ( strlen ( str_tmp ) != NULL )
+               if ( strlen ( str_tmp ) != 0 )
                     put_str ( str_tmp );
+                clean_str ( str2 );
                 del_sys_symb ( str2, str1 );
-                printf ( "after deleting system symbols string = %s;\n", str2 );
-//                printf ( "last symbol of string = %d in digital and %c in char;\n", str2 [ strlen ( str2 ) - 1 ],  str2 [ strlen ( str2 ) - 1 ] );
-                str2 [ strlen ( str2 ) -1 ] = ' ';
                 clean_str ( str_tmp );
                 strcpy ( str_tmp, str2 );
                 
                 clean_str ( str2 );
                 clean_str ( str1 );
+                prev = 1;
+                concat = 0;
             }
             else {
+               str_tmp [ strlen ( str_tmp ) - 1 ] = ' '; 
                strcat ( str_tmp, str1 );
-               put_str ( str_tmp );
                clean_str ( str1 );
+               del_sys_symb ( str1, str_tmp );
+               clean_str ( str_tmp );
+               strcpy ( str_tmp, str1 );
+               clean_str ( str1 );
+               prev = 0;
+               concat = 1;
             };
         };
+   }
+
+    put_str ( str_tmp );
     fclose ( source_file );
-    }
+
 }
 
 void  clean_str ( char str [] ) {
 
     short int i = 0;
-
-    for ( i = 0; i < strlen ( str ); i++ ){
+    for ( i = 0; i < 800; i++ ){
         str [ i ] = 0;       
     }
-    str [ 0 ] = EOF;
 }
 
 void del_sys_symb ( char str_to [], char str_from [] ) {
@@ -83,8 +91,23 @@ void put_str ( char str [] ) {
     fputs ( str, tmp_file );
     fclose ( tmp_file );
 
-    printf ( "putted string = %s;\n", str );
 
 }
 
+void clear_first_string ( char str_to [], char str_from [] ) {
 
+    short int i = 0;
+    short int j = 0;
+    short int position = 0;
+
+    for ( i = 0; ( i < strlen ( str_from ) ) || ( position > 0 ) ; i++ ) {
+        if ( str_from [ i ] == 'p' && str_from [ i + 1 ] == 'a' && str_from [ i + 2 ] == 'g' && str_from [ i + 3 ] == 'e' ) 
+            position = i + 12;
+    }
+    
+    for ( i = position, j = 0; i < strlen ( str_from ); i++, j++ ){
+
+        str_to [ j ] = str_from [ i ];
+    }
+
+}
--- test/Oleg_Moscow/velo/tmp_2/out.sh
+++ test/Oleg_Moscow/velo/tmp_2/out.sh
@@ -1,7 +1,16 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
-for i in `seq 1 14`
+for i in `seq 10 12`
 do
 #    awk '/company/ {for(i=1; i<=5; i++) {getline; print}}' $i |grep -v div >"$i"_parsed
-    awk '/company/ {for(i=1; i<=5; i++) {getline; print}}' $i >"$i"_parsed_2    
+#    cat $i |grep company > tmp;
+#    awk '/company/ {for(i=0; i<=5; i++) {getline; print}}' $i >"$i"_parsed_2    
+#    awk '/a href="http:\/\/www.list-org.com/ {for(i=1; i<=5; i++) {getline; print}}' $i >"$i"_parsed_2    
+    sed -n /company/,+5p $i >"$i"_parsed_3
+    rm source_file;
+    rm tmp_file;
+    cp "$i"_parsed_3 source_file;
+    ./rebuild;
+    cp tmp_file "$i"_parsed_4
+
 done
