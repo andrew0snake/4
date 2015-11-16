38a0062f453a6d831f75ac50682bcfc08d7d86b4
Andrew0snake <andrew000snake@gmail.com>
2015-11-17 00:22:24 +0600

    20151117 started parser

---
+++ test/Alex/convert.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#for i in `cat string|sed -e 's/\(.\)/\1\n/g'`
+#do 
+#    echo "'"$i"'";
+#done
+
+rm string_converted;
+rm string_tmp;
+
+foo=`cat string`
+for (( i=0; i<${#foo}; i++ )); 
+do
+   echo "'"${foo:$i:1}"'";
+   echo "'"${foo:$i:1}"'">>string_tmp;
+done
+
+cat string_tmp |perl -pi -e 's/\n/,/g' > string_converted;
+rm string_tmp;
+
+
