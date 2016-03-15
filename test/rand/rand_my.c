077046110bf8747318c4e0cbadc6ddb576015d3a
Andrew from rosbt <help@rosbt.com>
2016-03-15 17:30:27 +0600

    20160315 rand work

--- test/rand/rand_my.c
+++ test/rand/rand_my.c
@@ -1,17 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 //#define RAND_MAX 150
 #define A_S 10000
-//define external values
+//defining external values
 int string_count = 0;
 int random_array [ A_S ];
+int count_of_names = 0;
+
+//end of defining values
 
 int countlines(char *filename);
 int gen_rand ( unsigned short int size ); 
 void full_rand_array ( int size_of_array, int rand_array [ A_S ] );
 void print_array ( int rand_array [ A_S ] );
-
+void getline_my ( char line [], int size );
+int atoi_my ( char line [] ); 
+void clear_array ( int array [ A_S ] );
+void clear_string ( char array [ A_S ] );
+void full_list_of_names ( void );
+void get_numbered_line_of_file ( char line [ A_S ], int number );
 
 void main () {
 
@@ -23,15 +32,20 @@ void main () {
     int min = 0;
 
     char keywords_file [ 100 ] = "keywords";
+    char temp [ A_S ];
 
     string_count = countlines ( keywords_file );
 //    printf("LINES: %d\n",countlines(argv[1]));         
     printf ( "number of string in file keywords = %d;\n", string_count );
-    rez2 = gen_rand ( string_count );
+//    full_rand_array ( string_count, random_array );
 
-    printf ( "rez2 = %d;\n", rez2 );
+    clear_string( temp );
+    printf ( "Input count of names:\n" );
+    getline_my ( temp, A_S );
+    count_of_names = atoi_my ( temp );    
+    printf ( "Count of names = %d;\n", count_of_names );
 
-    full_rand_array ( string_count, random_array );
+    full_list_of_names ();
 
 }
 
@@ -77,8 +91,10 @@ int countlines ( char *filename ) {
     };
 */
     lines++;
-    while ( ( ch = fgetc ( fp ) ) != EOF ) {
+//    while ( ( ch = fgetc ( fp ) ) != EOF ) {
+    while ( !feof ( fp ) ) {
 //        printf ( "getted char = %c;\n", ch );
+        ch = fgetc ( fp );
         if ( ch == '\n' )
             lines++;
     };
@@ -94,6 +110,7 @@ void full_rand_array ( int size_of_array, int rand_array [ 10000 ] ) {
     char par = 0;
     int temp = 0;
 
+    printf ( "Fulling of random array..." );
     for ( i = 0; i < size_of_array; i++ ) {
         exit = 0;
         while ( exit == 0 ) {
@@ -113,9 +130,9 @@ void full_rand_array ( int size_of_array, int rand_array [ 10000 ] ) {
                 exit = 0;
             };
         };
-        printf ( "at step %d rand_array [ %d ] = %d;\n", i, i, rand_array [ i ] );
+//        printf ( "at step %d rand_array [ %d ] = %d;\n", i, i, rand_array [ i ] );
     };
-
+    printf ( "is finished.\n" );
 
 }
 
@@ -126,6 +143,101 @@ void print_array ( int rand_array [ A_S ] ) {
     for ( i = 0; i < A_S; i++ ) {
         printf ( "rand_array [ %d ] = %d;\n", i, rand_array [ i ] );
     };
+
+}
+
+void getline_my ( char line [], int size ) {
+    int i = 0;
+    int c = 0;
+
+    for ( i = 0; ( ( c = getchar () ) != EOF ) && c != '\n'; ++i ){
+        line [ i ] = c;
+    };
+}
+
+int atoi_my ( char s [] ) {
+
+    int i = 0;
+    int n = 0;
+    int sign = 0;
+
+    for ( i = 0; isspace ( s [ i ] ); ++i )
+        ;
+
+    sign = ( s [ i ] == '-' ) ? ( -1 ) : 1;
+    if ( s [ i ] == '+' || s [ i ] == '-' )
+        ++i;
+    for ( n = 0; isdigit ( s [ i ] ); ++i )
+        n = n * 10 + ( s [ i ] - '0' );
+    
+    return sign * n;
+
+}
+
+
+void clear_string ( char array [ A_S ] ) { 
+
+    int i = 0;
+    
+    for ( i = 0; i < A_S; i++ ) {
+        array [ i ] = 0;
+    };
+//    printf ( "String is cleared.\n" );
+}
+
+
+void full_list_of_names ( void ) {
+
+    int i = 0;
+    int pointer = 0;
+    int step = 0;
+    char temp_string [ A_S ];
+    char concat_string [ A_S ];
+
+    printf ( "get first %d random strings of file keywords:\n", count_of_names );
+
+    pointer = 0; 
+    step = 0;
+    full_rand_array ( string_count, random_array );
+    while ( step < count_of_names ) {
+        if ( pointer == string_count ) {
+            full_rand_array ( string_count, random_array );
+            pointer = 0;
+        }
+        
+        clear_string ( temp_string );
+        printf ( "at step %d in get_numbered_line_of_file send position %d;\n", pointer, random_array [ pointer ] );
+        get_numbered_line_of_file ( temp_string, random_array [ pointer ] );
+        pointer++;
+        step++;
+        
+        if ( strlen ( concat_string ) < 150 ) {
+            strcat ( concat_string, temp_string );
+        }
+    }
+
+
+
+
+
+}
+
+void get_numbered_line_of_file ( char line [ A_S ], int number ) {
+
+    int i = 0;
+    char temp [ A_S ];
+
+    FILE *f = fopen ( "keywords", "r" );
+
+    for ( i = 0; i < number; i++ ) {
+        clear_string ( temp );
+        fgets ( temp, 150, f );
+
+    };
+    printf ( "temp [ strlen ( temp ) ] = %c;\n",  temp [ strlen ( temp ) ] );
+    printf ( "after cycle position = %d; and getted string = %s;\n", i, temp );
+
+    fclose ( f );
 
 }
 
---
+++ test/randpass_old.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSIZE_STRING 10000
+
+int atoi_my ( char s [ MAXSIZE_STRING ] );
+void getline_my ( char s [ MAXSIZE_STRING ] );
+void clear_string ( char string [ MAXSIZE_STRING ] );
+//int gen_rand ( int argc, char **argv, int size );
+int gen_rand ( unsigned short int size );
+
+const char source [ 82 ] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','@','#','$','%','&','*','(',')','!','@','#','$','%','&','*','(',')'};
+
+void main () {
+ 
+    int i = 0;
+    int len_pass = 0;
+    int size_int = 0;
+    unsigned short int len_endur = 0;
+    short int tmp = 0;
+    char exit = 0;
+    char endurance [ MAXSIZE_STRING ];
+    char size [ MAXSIZE_STRING ];
+    char password [ MAXSIZE_STRING ];
+
+    printf ( "source [ %d ] = %c;\n", 61, source [ 61 ] );
+    while ( exit == 0 ) {
+        clear_string ( size );
+        printf ( "Input lenght of string: ");
+        getline_my ( size );
+        len_pass = atoi_my ( size );
+ 
+        if ( len_pass < 1 ) {
+            printf ( "Wrong number, try again. " );
+            exit = 0;
+        }
+        else {
+            printf ( "Lenght of string = %d;\n", len_pass );
+            exit = 1;
+        };
+    };
+
+    printf ( "What kind of endurance of password& Strong ( s ), middle ( m )  or easy ( e )?\n" );
+    exit = 0;
+    while ( exit == 0 ) {
+        getline_my ( endurance );
+        if ( endurance [ 0 ] == 's' ) {
+            len_endur = 79;
+            exit = 1;
+        }
+        else {
+            if ( endurance [ 0 ]  == 'm' ) {
+                len_endur = 61;
+                exit = 1;
+ 
+            }
+            else {
+                if ( endurance [ 0 ] = 'e') {
+                    len_endur = 35;
+                    exit = 1;
+                }
+                else {
+                    printf ( "Wrong input, try again.\n" );
+                    exit = 0;
+                    clear_string ( endurance );
+                };
+            };
+        };
+    };   
+
+    for ( i = 0; i < len_pass; i++ ) {
+        tmp = gen_rand (  len_endur ); 
+        password [ i ] = source [ tmp ];
+        tmp = 0;
+
+    };
+    password [ i ] = '\0';
+    printf ( "Getted string = %s\n", password );
+
+}
+
+void clear_string ( char string [ MAXSIZE_STRING ] ) {
+
+    int i = 0;
+
+    while ( i < MAXSIZE_STRING ) {
+//    while ( i < strlen ( string ) ) {
+        string [ i ] = 0;
+        i++;
+    }
+
+}
+
+void getline_my ( char s [ MAXSIZE_STRING ] ) {
+
+    int c = 0;
+    int i = 0;
+    int lim = MAXSIZE_STRING;
+    
+    while ( ( --lim > 0 ) && ( ( c = getchar() ) != EOF ) && c != '\n' )
+        s [ i++ ] = c;
+
+    if ( c == '\n' ){
+        s [ i++ ] = '\n';
+    };
+
+    s [ i ] = '\0';
+    
+
+}
+
+int atoi_my ( char s [ MAXSIZE_STRING ] )
+
+{
+    int i = 0;
+    int n = 0;
+    int sign = 0;
+
+    for ( i = 0; isspace ( s [ i ] ); ++i )
+        ;
+
+    sign = ( s [ i ] == '-' ) ? ( -1 ) : 1;
+    if ( s [ i ] == '+' || s [ i ] == '-' )
+        ++i;
+    for ( n = 0; isdigit ( s [ i ] ); ++i )
+        n = n * 10 + ( s [ i ] - '0' );
+    
+    return sign * n;
+
+}
+
+//int gen_rand ( int argc, char **argv, int size ) {
+int gen_rand ( unsigned short int size ) {
+
+    unsigned int randval;
+    FILE *f;
+//for bsd uncomment this string 
+//    f = fopen ( "/dev/random", "r" );
+
+//for linux using urandom
+    f = fopen ( "/dev/urandom", "r" );
+    fread ( &randval, sizeof ( randval ), 1, f );
+    fclose ( f );
+ 
+    while ( randval > size ) {
+        randval /= 11;
+    };
+
+    return randval;
+ }
+
