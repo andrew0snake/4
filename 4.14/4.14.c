70eb8bfa1be24a2aaf782376b9dbb50731502832
Andrew0snake <andrew000snake@gmail.com>
2016-04-03 14:16:34 +0700

    macro block made

--- 4.14/4.14.c
+++ 4.14/4.14.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-#define one_float 1.0
-#define two_float 2.0
-#define one_plus_two_float one_float+two_float
-
-#define type_int int
-#define type_double double
-#define type(a,b) ((a)||(b)?(a):(b))
-//#define swap(type(a,b),c,d) 
-
 #define is_a(a,b) (a||b?a:b)
 #define is_int(a,b) ()
 
-#define swap(t,x,y) ((t)(z);swap3((t)(z),(t)(x),(t)(y)))
+//#define swap(t,A,B) ((t)(C);swap3((t)(C),(t)(A),(t)(B)))
+#define swap(t,A,B) ((t)(C);printf("(t)(C),(t)(A),(t)(B"))
 #define swap2(t,x,y) printf("%f,%f.\n",(t)(x),(t)(y))
 #define swap3(z,x,y) (z=x,x=y,y=z)
+
+#define def(T,V) T V
+#define op1(A) ((A)=(A)+3)
+#define op2(B) ((B)=(op1(B))-10)
+#define print_def(T,A) (def(T,A);printf( " = %f;%d;\n", A))
+#define inval(T,A) T B = 5; printf ( "external_value+internal_value=%f;\n", A+B );
+#define swap(T,A,B) {T Z ; Z = A; A = B; B = Z;}
+
+#define var_args(formatting,...) printf (formatting,__VA_ARGS__)
+
 
 void main () {
 
@@ -22,26 +24,35 @@ void main () {
     char string_dbl [] = "double";
     char rez [ 10 ] ;
     
-    int x = 0;
-    int y = 0;
+    int x = 8;
+    int y = 1;
+    int z = 2;
+    float f = 3.0;
+    float g = 1.0;
+    float h = 2.0;
 
-    x = 9;
-    y = 13;
+    op1(x);
+    printf ( "after op1 x = %d;\n", x );
 
-//    printf ( "is_a result = %d;\n", is_a ( x, y ) );
+    op2(x);
+    printf ( "after op2 x = %d;\n", x );
 
-    printf ( "one_float = %f; two_float = %f; one_float + two_float = %f;\n", one_float, two_float, one_plus_two_float );
+//    print_def(float,x);
 
-//    printf ( "type = %d;\n", type(type_int,type_double) );
+    inval(float,y);
 
-//    rez = gettype_my ( string_int );
+    printf ( "before swap in float f = %f; g = %f;\n", f, g );
+    swap(float,f,g)
+    printf ( "after swap in float f = %f; g = %f;\n", f, g );
 
-//    swap(int,5,3);
-//    swap2(float,5.0,3.5);
+    printf ( "before swap in intf = %d; g = %d;\n", y, z );
+    swap(int,y,z)
+    printf ( "after swap in int f = %d; g = %d;\n", y, z );
 
-    printf ( "before swap x = %d; y = %d;\n", x, y );
-    swap(int,5,3);
-//    swap3(x,y);
-    printf ( "after swap ( x, y ) x = %d; y = %d;\n", x, y  );
+    char *formatting = "\n%s%s\n\n";
+    char *string1 = "string one ";
+    char *string2 = "string two ";
+
+    var_args(formatting,string1,string2);
 
 }
